b5f9011525ef59b1940bfb4da80e3356295be8e6
DomZei <dominikzeise91@outlook.com>
2023-08-26 14:27:32 +0200

    reactive and ref

--- chapter1/src/App.vue
+++ chapter1/src/App.vue
@@ -1,47 +1,37 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {reactive, ref} from 'vue';
+
+const person = reactive({
+  firstName: "Dominik",
+  lastName: "Zeise",
+})
+
+function updatePerson(){
+  person.firstName = "Elon";
+  person.lastName = "Musk";
+}
+
+const people = reactive(["Elon Musk", "Marc Zuckerberg"]);
+
+const count = ref(0);
+
+console.log(person)
+console.log(count)
+function incrementCount(){
+  count.value++;
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p>Person: {{ person }}</p>
+  <button @click="updatePerson">Update Person</button>
+  <hr />
+  <p>People: {{ people }}</p>
+  <hr />
+  <p>Count: {{ count }}</p>
+  <button @click="incrementCount">Count</button>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
